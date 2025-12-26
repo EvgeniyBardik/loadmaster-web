2d231e89cea6c7df3aebf50d86ec4621c1ddba9c
Bardyk Yevhen <groove8478@gmail.com>
2025-12-26 15:47:16 +0100

    remove test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
 │  NextJS UI  │ ◄──────────────► │  NestJS API  │
 └─────────────┘                  └───────┬──────┘
                                          │
-                                    RabbitMQ1
+                                    RabbitMQ
                                          │
                           ┌──────────────┼──────────────┐
                           ▼              ▼              ▼
